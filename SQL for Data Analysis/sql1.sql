8760a99de36e9e88283cb7c7a23773e5288b54da
SId02 <31335947+SId02@users.noreply.github.com>
2022-06-02 15:20:57 +0530

    add
    add

---
+++ SQL for Data Analysis/sql1.sql
@@ -0,0 +1,19 @@
+--	Create	Database
+create database	youtube;
+--	use	database
+use	youtube;
+--	Show	tables
+show tables;
+
+##creatingtable
+
+create	table	employee(
+empid	int,
+empname	varchar(50),
+empadd	varchar(200),
+depid	int
+)
+
+select	*	from	employee;
+
+insert	into	employee	values(1,'Sat','HK',1)
